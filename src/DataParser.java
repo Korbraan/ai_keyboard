3b31c04144e7344bcc8d5c7924c1bd8c0f2e0eff
Joffrey Da Rocha <steakdeniche@gmail.com>
2016-12-26 13:31:49 +0100

    Parser Update
    Lecture du fichier dat fonctionnelle

--- src/DataParser.java
+++ src/DataParser.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
  */
 public class DataParser {
     private File data_file;
-    private int[][] occurences;
+    private long[][] occurences;
 
     public DataParser(String data_file_path) {
         this.data_file = new File(data_file_path);
-        this.occurences = new int[26][26];
+        this.occurences = new long[26][26];
     }
 
     public void ParseData() {
@@ -21,23 +21,24 @@ public class DataParser {
             // For each line in the file, we process the data
             int j = 0;
             while ((line = br.readLine()) != null) {
-                // If the ligne is not a comment
+                // If line is not a comment
                 if (line.charAt(0) != '#') {
                     String[] string_array_line = line.split(" ");
                     for(int i = 0; i < string_array_line.length; i++) {
-                        occurences[j][i] = Integer.parseInt(string_array_line[i]);
+                        occurences[j][i] = Long.parseLong(string_array_line[i]);
                     }
+                    j++;
                 }
-                j++;
+
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            e.printStackTrace();s
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public int[][] getOccurences() {
+    public long[][] getOccurences() {
         return occurences;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,26 @@
  * Created by cremond on 17/12/16.
  */
 public class Main {
-    String data_file_path = "../datafiles/bigramFreqEng-Occurrence.dat";
 
-    DataParser data_parser = new DataParser(data_file_path);
+    public static void main (String [] args){
+
+        System.out.println("Wesh je me lance");
+
+        String data_file_path = "./datafiles/bigramFreqEng-Occurrence.dat";
+
+        DataParser data_parser = new DataParser(data_file_path);
+
+        data_parser.ParseData();
+
+        long[][] occurences = data_parser.getOccurences();
+
+        for(int i = 0; i<occurences.length;i++){
+            System.out.println("Tableau d'occurences sauvegardé");
+            System.out.println("---------------------------------------");
+            for(int j = 0; j<occurences.length;j++){
+                System.out.print(occurences[i][j]+ " ");
+            }
+        }
+    }
 
 }
